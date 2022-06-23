de66cc2fe62e7826c476224cf41f3a99c3378c1a
hel8er <helber-alx@mail.ru>
2022-06-24 02:24:21 +0300

    pages

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <TheNavigation />
+<NuxtPage />
+
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+<TheNavigation />
+<div>
+<div class="container">
+    <p><h1>Index Page</h1></p>
+    </div></div>
+</template>
+
+<style scoped>
+.container {
+    margin-top: 2rem;
+    margin-left: 5rem;
+}
+</style>
