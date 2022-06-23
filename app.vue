adc0b4d006ccaf61ff67b79559777850f1d38d3d
hel8er <helber-alx@mail.ru>
2022-06-24 01:31:56 +0300

    TheNavigation

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <TheNavigation />
   </div>
 </template>
---
+++ components/TheNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <nav class="nav">
+        <div class="title">
+            <h1>App Title</h1>
+        </div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+
+    </nav>
+
+</template>
+
+<style scoped>
+.nav {
+    color: white;
+    align-items: center;
+    height: 48px;
+    display: flex;
+    background-color: rgb(46, 67, 160);
+    justify-content: space-between;
+}
+
+.title {
+    margin-left: 1rem;
+}
+
+ul {
+    display: flex;
+}
+
+ul li {
+    display: flex;
+    margin-right: .5rem;
+}
+</style>
